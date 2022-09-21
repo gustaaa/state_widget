05328749fa1f49557a8a8689574794bfb8cd4a74
gustaaa <gustayo24@gmail.com>
2022-09-21 19:22:34 +0700

    statefull_widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,34 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController etInput = new TextEditingController();
+
+  double inputUser = 0;
+
+  double kelvin = 0;
+
+  double reamor = 0;
+
+  konversi() {
+    setState(() {
+      inputUser = double.parse(etInput.text);
+      reamor = 4 / 5 * inputUser;
+      kelvin = inputUser + 273;
+    });
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -29,6 +51,7 @@ class MyApp extends StatelessWidget {
                   hintText: 'Masukkan Angka Dalam Celcius',
                   labelText: 'Masukkan Angka Dalam Celcius',
                 ),
+                controller: etInput,
                 keyboardType: TextInputType.number,
                 // inputFormatters: <TextInputFormatter>[
                 //   FilteringTextInputFormatter.digitsOnly
@@ -45,7 +68,7 @@ class MyApp extends StatelessWidget {
                       ),
                       Padding(padding: EdgeInsets.all(10)),
                       Text(
-                        "150",
+                        '$kelvin',
                         style: TextStyle(fontSize: 30),
                       ),
                     ],
@@ -58,7 +81,7 @@ class MyApp extends StatelessWidget {
                       ),
                       Padding(padding: EdgeInsets.all(10)),
                       Text(
-                        "200",
+                        '$reamor',
                         style: TextStyle(fontSize: 30),
                       ),
                     ],
@@ -71,7 +94,7 @@ class MyApp extends StatelessWidget {
                 child: TextButton(
                   style:
                       TextButton.styleFrom(backgroundColor: Colors.lightBlue),
-                  onPressed: () {},
+                  onPressed: konversi,
                   child: Text(
                     "Konversi Suhu",
                     style: TextStyle(
